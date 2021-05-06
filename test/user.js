9981107e501ef67d116b0830e7093c45da124b38
Nana Baah Akuffu <nanaakuffu@gmail.com>
2021-05-06 18:55:51 +0000

    Added the logout tests

--- test/user.js
+++ test/user.js
@@ -43,6 +43,24 @@ describe('Users', () => {
                     done();
                 });
         });
+        it('it should not POST register without confirm_password', (done) => {
+            let user = {
+                first_name: 'John',
+                last_name: 'Smith',
+                email: "johnsmith@example.com",
+                contact_number: "1112223331",
+                password: 'password'
+            }
+            chai.request(server)
+                .post('/api/v1/users/register')
+                .send(user)
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    done();
+                });
+        });
         it('it shoule save a user with all the correct data', (done) => {
             let user = {
                 first_name: 'John',
@@ -68,7 +86,7 @@ describe('Users', () => {
       * Test the /POST route
       */
     describe('POST /api/v1/users/login', () => {
-        it('it should not POST login without email', (done) => {
+        it('it should not login without email', (done) => {
             let user = {
                 password: 'password'
             }
@@ -81,6 +99,45 @@ describe('Users', () => {
                     res.body.should.have.property('errors');
                     done();
                 });
+        });
+        it('it should not login without password', (done) => {
+            let user = {
+                email: 'johnsmith@example.com'
+            }
+            chai.request(server)
+                .post('/api/v1/users/login')
+                .send(user)
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    done();
+                });
+        });
+        it('it should not login with the wrong password', (done) => {
+            const user = new User({
+                first_name: 'John',
+                last_name: 'Smith',
+                contact_number: "1112223331",
+                email: "johnsmith@example.com",
+                password: bcrypt.hashSync("password", 8),
+            });
+            user.save((err, user) => {
+                let user_data = {
+                    email: "johnsmith@example.com",
+                    password: "example1"
+                }
+                chai.request(server)
+                    .post('/api/v1/users/login')
+                    .send(user_data)
+                    .end((err, res) => {
+                        res.should.have.status(401);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('api_token');
+                        res.body.should.have.property('message').eql('Password provided is invalid!');
+                        done();
+                    });
+            });
         });
         it('it should POST login with the correct data', (done) => {
             const user = new User({
@@ -109,6 +166,21 @@ describe('Users', () => {
                         done();
                     });
             });
+        });
+    });
+
+    describe('POST /api/v1/users/logout', () => {
+        it('it should not logout without token', (done) => {
+            chai.request(server)
+                .post('/api/v1/users/logout')
+                .send()
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message');
+                    res.body.should.have.property('message').eql('Access denied. No credentials sent!');
+                    done();
+                });
         });
     });
 
