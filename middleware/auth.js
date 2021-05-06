94bad849b1df44b258cfb19dc652cc235100d8fa
Nana Baah Akuffu <nanaakuffu@gmail.com>
2021-05-06 18:55:06 +0000

    Updated the auth middleware for effective authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,22 +2,38 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/sys_config");
 const db = require("../db/db");
 const User = db.user;
+const Token = db.token;
 
-// Initialise the auth middleware
+// Build the auth middleware
 const auth = () => {
     return async (request, response, next) => {
-        let token = request.headers["x-access-token"];
+        try {
+            const authHeader = request.headers.authorization;
+            const bearer = 'Bearer ';
 
-        if (!token) {
-            return response.status(403).send({ message: "No token provided!" });
+            if (!authHeader || !authHeader.startsWith(bearer)) {
+                return response.status(401).send({ message: 'Access denied. No credentials sent!' });
+            }
+
+            const token = authHeader.replace(bearer, '');
+
+            // Verify Token
+            const decoded = jwt.verify(token, config.jwt_secret);
+            const user = await User.findById(decoded.id);
+            const token_exists = await Token.findOne({ user_id: decoded.id });
+
+            if (!token_exists) {
+                return response.status(401).send({ message: 'Authentication failed!' });
+            }
+
+            // if the user has permissions
+            request.currentUser = user;
+            next();
+
+        } catch (e) {
+            e.status = 401;
+            next(e);
         }
-
-        const decoded = jwt.verify(token, config.jwt_secret);
-
-        const user = await User.findById(decoded.id);
-
-        request.current_user = user;
-        next();
     };
 };
 
